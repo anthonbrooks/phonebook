6188741604bc10ed9c6fc4b5672b39a22dd1eebf
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-02-20 15:05:13 -0700

    Add phone number input

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 function App(props) {
   const [people, setPeople] = useState(props.people) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('');
 
   const handleAddContact = (e) => {
     e.preventDefault();
@@ -11,16 +12,22 @@ function App(props) {
       alert(`${newName} is already added to the phonebook`)
     } else {
         const contact = {
-        name: newName
+        name: newName,
+        phone: newNumber
       }
 
       setPeople(people.concat(contact));
       setNewName('');
+      setNewNumber('');
     }
   }
 
-  const handleChange = (e) => {
+  const handleNameChange = (e) => {
     setNewName(e.target.value);
+  }
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
   }
 
   return (
@@ -28,7 +35,10 @@ function App(props) {
       <h2>Phonebook</h2>
       <form onSubmit={handleAddContact}>
         <div>
-          name: <input value={newName} onChange={handleChange}/>
+          name: <input value={newName} onChange={handleNameChange} required />
+        </div>
+        <div>
+          phone number: <input type='tel' value={newNumber} onChange={handleNumberChange} required />
         </div>
         <div>
           <button type="submit">add</button>
@@ -36,7 +46,7 @@ function App(props) {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {people.map(person => <p key={person.name}>{person.name}</p>)}
+        {people.map(person => <p key={person.name}>{person.name} {person.phone}</p>)}
       </ul>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-const people = [{ name: 'Arto Hellas' }];
+const people = [{ name: 'Arto Hellas', phone: '0 123 4567' }];
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App people={people} />
